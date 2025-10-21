021b4abe4e539e80e0487760e3ed62fbce52a8e5
Amr <amr20012200131@hotmail.com>
2025-10-21 16:09:03 +0300

    added the app

---
+++ smart_rescue/safeNowApp/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    DOB = models.DateField()
+    role = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class CaseEmergency(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User, related_name="cases", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Services(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField()
+    owner = models.ForeignKey(User, related_name="services", on_delete=models.CASCADE)
+    updated_at = models.DateTimeField(auto_now=True)
