6301592b7e9b1ca836ef77b3efda894dd563f5b2
Amr <amr20012200131@hotmail.com>
2025-10-22 13:58:53 +0300

    added creat case page and its design

--- smart_rescue/safeNowApp/views.py
+++ smart_rescue/safeNowApp/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def create_case_page(request):
+    if not "id" in request.session:
+        messages.error(request, "You need to login first")
+        return redirect(index)
+    return render(request, 'create_case.html')
